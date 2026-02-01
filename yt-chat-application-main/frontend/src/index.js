866362b9ad228b73fbbe2a0e0d72a7529168cc49
ChandanKumar <chandanyadav2439@gmail.com>
2026-02-01 17:37:57 +0530

    add BrowserRouter for netlify spa routing

--- yt-chat-application-main/frontend/src/index.js
+++ yt-chat-application-main/frontend/src/index.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
-import store from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistStore } from 'redux-persist';
+import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import { BrowserRouter } from "react-router-dom";
 
 let persistor = persistStore(store);
 
-export const BASE_URL="http://localhost:8080"
+// ⚠️ production में localhost मत रखना
+export const BASE_URL = "http://localhost:8080";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
-        <Toaster />
+        <BrowserRouter>
+          <App />
+          <Toaster />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
